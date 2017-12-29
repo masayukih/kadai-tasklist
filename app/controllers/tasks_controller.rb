d557801ec8597f3656864c23c5e0e6fc91d74157
masayuki.higashi <masayuki.h1109@gmail.com>
2017-12-29 13:49:10 +0000

    forth submission (login/logout)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,25 @@
 class TasksController < ApplicationController
+	before_action :require_user_logged_in, only: [:show, :edit, :new]
 	before_action :set_task, only: [:show, :edit, :update, :destroy]
 	
+	include SessionsHelper
+	
 	def index
-		@tasks = Task.order(created_at: :desc).all.page(params[:id]).per(10)
+		if logged_in?
+			@user = current_user
+			@tasks = current_user.tasks.order("id DESC").page(params[:page]).per(10)
+		end
 	end
 	
 	def show
 	end
 	
 	def new
-		@task = Task.new
+		@task = current_user.tasks.build
 	end
 	
 	def create
-		@task = Task.new(task_params)
+		@task = current_user.tasks.build(task_params)
 		
 		if @task.save
 			flash[:success] =  "taskは正常に投稿されました"
@@ -39,18 +45,23 @@ class TasksController < ApplicationController
 	
 	def destroy
 		@task.destroy
-		
 		flash[:success] = "taskは正常に削除されました"
 		redirect_to tasks_url
 	end
 	
 	private
 	
+	def require_user_logged_in
+		unless logged_in?
+			redirect_to login_url
+		end
+	end
+	
 	def set_task
-		@task = Task.find(params[:id])
+		@task = current_user.tasks.find(params[:id])
 	end
 	
 	def task_params
-		params.require(:task).permit(:status,:content)
+		params.require(:task).permit(:status, :content)
 	end
 end
